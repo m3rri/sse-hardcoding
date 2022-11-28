a45686298a25691f81fe857e15ba66c35a7425b3
Kim Hye Ri <37619441+m3rri@users.noreply.github.com>
2022-11-28 11:38:32 +0900

    Create MyFilterConfig.java

---
+++ src/main/java/m3rri17/test/flux/MyFilterConfig.java
@@ -0,0 +1,29 @@
+package seegene.test.flux;
+
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class MyFilterConfig {
+    private final EventNotify eventNotify;
+
+    @Bean
+    public FilterRegistrationBean<Filter> addFilter(){
+        System.out.println("filter 1");
+        FilterRegistrationBean<Filter> bean = new FilterRegistrationBean<>(new MyFilter(eventNotify));
+        bean.addUrlPatterns("/sse");
+        return bean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<Filter> addFilter2(){
+        System.out.println("filter 2");
+        FilterRegistrationBean<Filter> bean = new FilterRegistrationBean<>(new MyFilter2(eventNotify));
+        bean.addUrlPatterns("/add");
+        return bean;
+    }
+}
